e9dac92e362c066e10879c442530cee0be7a3943
mastersyphodias <mastersyphodias@3ec7dc38-067b-11df-8e78-e7f5014194b1>
2010-01-21 11:31:53 +0000

    motherfucking idiot

--- EA2010/src/guarana/MultiStartLocalSearch.java
+++ EA2010/src/guarana/MultiStartLocalSearch.java
@@ -1,8 +1,11 @@
 package guarana;
+import java.util.ArrayList;
 
 public class MultiStartLocalSearch {
 	
-	public static Partition search_fixedNOptima(LocalSearch s, int n, Graph g) {
+	public static SearchResult search_fixedNOptima(LocalSearch s, int n, Graph g) {
+		
+		ArrayList<Partition> alloptima = new ArrayList<Partition>();
 		
 		long start, end;		
 		Partition best = null, cur;
@@ -13,25 +16,28 @@ public class MultiStartLocalSearch {
 			g.initializePartition();
 			
 			cur = s.search(g);
-			System.out.println(cur.getScore());
-			
+						
 			if (best == null || cur.getScore() < best.getScore()) {
 				
 				// not sure the clone is needed, for safety
 				try {
 					best =(Partition) cur.clone();
 				} catch (CloneNotSupportedException e) {}
-			}						
+			}
+			
+			alloptima.add(cur); // it should not need cloning since every time the graph is initialized again 
 		}
 		
 		end = System.currentTimeMillis();
 		
 		System.out.println("Best found: "+best.getScore()+" elapsed: "+(end-start));
 		
-		return best;		
+		return new SearchResult(alloptima, best);		
 	}
 	
-	public static Partition search_fixedTime(LocalSearch s, int t, Graph g) {
+	public static SearchResult search_fixedTime(LocalSearch s, int t, Graph g) {
+		
+		ArrayList<Partition> alloptima = new ArrayList<Partition>();
 		
 		long start;		
 		Partition best = null, cur;
@@ -49,19 +55,61 @@ public class MultiStartLocalSearch {
 				try {
 					best =(Partition) cur.clone();
 				} catch (CloneNotSupportedException e) {}
-			}						
+			}
+			
+			alloptima.add(cur);
+			
 		} while (System.currentTimeMillis() < start + t);		
 		
 		System.out.println("Best found: "+best.getScore());
 		
-		return best;
+		return new SearchResult(alloptima, best);
 	}
+	
+
 	
 	public static void main(String args[]) {
 		Graph g = Util.makeGraphFromFile("G500.005");
 		
-		search_fixedTime(new FiducciaMattheyses(), 10000, g);
-		search_fixedTime(new KernighanLin(), 10000, g);
+		// this is the code that produces the output needed for point 1.
 		
+		// step 1 - find 1000 local optima and plot the hamming distance between them and the best found	
+		
+		System.out.println("STEP 1 -------------------------------- ");
+		
+		SearchResult s = search_fixedNOptima(new FiducciaMattheyses(), 1000, g);
+		
+		System.out.println("FM 1000 LO distances ------------------ ");
+		outHammingDistandScores(s);
+		
+		s = search_fixedNOptima(new KernighanLin(), 1000, g);
+		
+		System.out.println("KL 1000 LO distances ------------------ ");
+		outHammingDistandScores(s);
+		
+		// step 2 - run 1000 times MSLS fixed Noptima (1000) with FM and KL output the best scores found
+		System.out.println("STEP 2 -------------------------------- ");
+		
+		System.out.println("FM     -------------------------------- ");
+		for (int k = 0; k < 1000; k++) {
+			s = search_fixedNOptima(new FiducciaMattheyses(), 1000, g);
+			System.out.println(s.best.getScore());
+		}
+		
+		System.out.println("KL     -------------------------------- ");
+		for (int k = 0; k < 1000; k++) {
+			s = search_fixedNOptima(new FiducciaMattheyses(), 1000, g);
+			System.out.println(s.best.getScore());
+		}
+		
+		//search_fixedTime(new KernighanLin(), 10000, g);		
 	}
+	
+	private static void outHammingDistandScores(SearchResult s) {
+		for (Partition th : s.alloptima) {
+			System.out.println (Util.hammingDistance(s.best, th)+", "+th.getScore());
+		}
+	}
+	
+	
 }
